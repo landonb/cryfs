51b111c94c1fb34fec80a314f571dfbfda2c1b4f
Sebastian Messmer <messmer@cryfs.org>
2017-11-05 16:35:19 +0000

    Add note to run-clang-tidy.sh

--- run-clang-tidy.sh
+++ run-clang-tidy.sh
@@ -5,6 +5,9 @@
 # mkdir cmake && cd cmake && ../run-clang-tidy.sh
 # mkdir cmake && cd cmake && ../run-clang-tidy.sh -fix
 # mkdir cmake && cd cmake && ../run-clang-tidy.sh -export-fixes fixes.yaml
+#
+# If this fails with errors in scrypt, you need to run "make" in the build directory and let it run until scrypt is built. Our Makefiles call into theirs, and this is needed to generate some header files.
+# Clang-tidy will otherwise complain they're missing.
 
 set -e
 
