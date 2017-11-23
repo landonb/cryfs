6f723aec76789b997f1f95ff0dfe0a10280bb271
Sebastian Messmer <messmer@cryfs.org>
2017-11-23 02:50:55 +0000

    run-clang-tidy.sh works even if make wasn't called yet

--- run-clang-tidy.sh
+++ run-clang-tidy.sh
@@ -5,9 +5,6 @@
 # mkdir cmake && cd cmake && ../run-clang-tidy.sh
 # mkdir cmake && cd cmake && ../run-clang-tidy.sh -fix
 # mkdir cmake && cd cmake && ../run-clang-tidy.sh -export-fixes fixes.yaml
-#
-# If this fails with errors in scrypt, you need to run "make" in the build directory and let it run until scrypt is built. Our Makefiles call into theirs, and this is needed to generate some header files.
-# Clang-tidy will otherwise complain they're missing.
 
 set -e
 
@@ -15,6 +12,9 @@ NUMCORES=`nproc`
 
 # Run cmake in current working directory, but on source that is in the same directory as this script file
 cmake -DBUILD_TESTING=on -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "${0%/*}"
+
+# Build scrypt first. Our Makefiles call ino theirs, and this is needed to generate some header files. Clang-tidy will otherwise complain they're missing.
+make -j${NUMCORES} scrypt
 
 run-clang-tidy.py -j${NUMCORES} -quiet -header-filter "$(realpath ${0%/*})/(src|test)/.*" $@
 
